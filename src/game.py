c1218e6eba142d8230dab7f470f512b7a6f9044d
Poonam Jain <pjain3326@gmail.com>
2019-01-15 22:19:31 -0800

    edited

--- src/game.py
+++ src/game.py
@@ -99,45 +99,7 @@ if __name__ == "__main__":
     app.run()
 
 
-# length= (len(original)+3)
-#  
-# while length>trial:
-#     userinput=str.lower(input("Enter an alphabet: "))
-#      
-#     if not re.search("[A-Za-z]", userinput):
-#         print("This is not a letter", " ".join(guess))
-#         continue
-#     if userinput in lettersguessed:
-#         print("This letter already used once. ", " ".join(guess))
-#         continue
-#     else:
-#         lettersguessed.append(userinput)
-#     if len(userinput)>1:
-#         print("Enter only a single letter at a time")
-#          
-#     if userinput in original:
-#         while userinput in temp:
-#             counter= temp.index(userinput)
-#             guess[counter]= userinput
-#             temp.remove(userinput)
-#             temp.insert(counter, "_")
-#              
-#         counter=0
-#         for i in range(len(original)):
-#             if temp[i]=="_":
-#                 counter+=1
-#         if counter == len(original):
-#             print("You have won")
-#             print("correct word\t", word)
-#             break
-#         print("Correct\t", " ".join(guess), "\ttrials left: ", (length-trial))
-#              
-#     else:
-#          trial+=1
-#          print("Incorrect\t", " ".join(guess), "\ttrials left: ",(length-trial))  
-# else:
-#     print("You lost!! Try again")  
-#     print("Correct answer was: ",word)   
+ 
     
     
 
